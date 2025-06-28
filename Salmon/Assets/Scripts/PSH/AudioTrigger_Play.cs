837c50267efbda5bb6a694f192ddd8ab1aa93f9e
PARKSH0912 <kkqwert882950@gmail.com>
2025-06-28 12:12:38 +0900

    오디오 셋팅

---
+++ Salmon/Assets/Scripts/PSH/AudioTrigger_Play.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework.Constraints;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AudioTrigger_Play : MonoBehaviour
+{
+    public GameObject player;
+    public AudioManager audioManager;
+
+    public string audioClipName;
+
+    public void Start()
+    {
+        if (audioManager == null)
+        {
+            audioManager = AudioManager.Instance;
+
+        }
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+    }   
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player") && !string.IsNullOrEmpty(audioClipName))
+        {
+
+            // 사운드가 AudioManager에 존재하는지 확인
+            audioManager.PlayMusic(audioClipName);
+            Debug.Log($"[auidoClipName] 재생");
+
+        }
+    }
+
+}
